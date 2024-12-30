53fc1b4edb59b38a16df6e51237fc29babe5517b
Prapti Giri <praptigiri1770@gmail.com>
2024-12-30 14:34:09 +0545

    FirstProgram in java

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+public class FirstProgram{
+    
+    public static void main(String[] args) {
+        int x = 11;
+        if (x > 10) {
+            System.out.println("greater than 10");
+        }
+        else{
+            System.out.println("less than 10");
+        }
+    }
+}
